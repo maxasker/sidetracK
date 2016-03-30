376a5723cf570672b574e53df262c05230f54e5b
maxasker <maxasker@hotmail.com>
2016-03-30 11:14:11 +0200

    ändrat en massa mappar och skrivit kod i python

--- Kod/sidetrack.py
+++ Kod/sidetrack.py
@@ -1,7 +1,7 @@
 from bottle import run, route, template, request, static_file
-from os import listdir
 from bottle import static_file
 from bottle import url
+import os
 
 @route('/')
 def index():
@@ -11,6 +11,22 @@ def index():
 def threadshow():
     return template("thread")
 
+@route('/savenewthread', method="POST")
+def savethread():
+    title = request.forms.get("title")
+    text = request.forms.get("text")
+    newpath = r'static/threads/{0}'.format(title) 
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+
+    newthreadtitlefile = open("{0}/tstitle.txt".format(newpath), "w")
+    newthreadtitlefile.write(text)
+    newthreadtitlefile.close()
+
+    newthreadtextfile = open("{0}/tstext.txt".format(newpath), "w")
+    newthreadtextfile.write(title)
+    newthreadtextfile.close()
+    return template("singlethread", tspath=newpath)
 
 @route('<filename:re:.*\.css>',name='static')
 def css(filename):
@@ -18,3 +34,4 @@ def css(filename):
     return static_file(filename,root='./static',mimetype='text/css')
 
 run(host='localhost', port=9057, debug=True, reloader=True)
+
