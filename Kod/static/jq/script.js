59375f5c955777524b6184e44f42f16b3868c82d
af7334 <jacoblowe@Jacobs-MacBook-Pro-2.local>
2016-05-03 13:50:51 +0200

    Push CSS
    Pushat ny css kod.

--- Kod/static/jq/script.js
+++ Kod/static/jq/script.js
@@ -28,7 +28,7 @@ function showReplyBox(id,topicid,thispage,asd)
       if (asd==0) {
      var elem = '<form id ="form1" action = "'+formLink+'" enctype="multipart/form-data" method="post"><input type="hidden" name="comid" value="';
 elem = elem+ id + '">';
-elem = elem + '<a id ="jsclose" href="javascript:void(0)" onclick="showReplyBox(44,142,1);">Close</a><textarea name="text" placeholder="Title" maxlength="100" rows="5" cols="50">Title</textarea><hr><input id="jsaddfile" type="file" name="commentimg" accept="image/*"><input id="jsaddcomment" type="submit" value="Spara innehåll"></form>';
+elem = elem + '<a id ="jsclose" href="javascript:void(0)" onclick="showReplyBox(44,142,1);">Close</a><textarea name="text" placeholder="Comment" maxlength="100" rows="5" cols="50"></textarea><hr><input id="jsaddfile" type="file" name="commentimg" accept="image/*"><input id="jsaddcomment" type="submit" value="Spara innehåll"></form>';
 
     var empty = "";  
 
@@ -59,7 +59,7 @@ function showReplyBox2(id,topicid,thispage,asd,currentElement)
     
 var elem = '<form id ="form1" action = "'+mapp+'" enctype="multipart/form-data" method="post"><input type="hidden" name="comid" value="';
 elem = elem+ id + '">';
-elem = elem + '<a id ="jsclose" href="javascript:void(0)" onclick="showReplyBox(44,142,1);">Close</a><textarea name="text" placeholder="Title" maxlength="100" rows="5" cols="50">Title</textarea><hr><input id="jsaddfile" type="file" name="commentcommentimg" accept="image/*"><input id="jsaddcomment" type="submit" value="Spara innehåll"></form>';
+elem = elem + '<a id ="jsclose" href="javascript:void(0)" onclick="showReplyBox(44,142,1);">Close</a><textarea name="text" placeholder="Comment" maxlength="100" rows="5" cols="50"></textarea><hr><input id="jsaddfile" type="file" name="commentcommentimg" accept="image/*"><input id="jsaddcomment" type="submit" value="Spara innehåll"></form>';
     var empty = ""; 
 
     $("#elem").animate({'left':'300','opacity':'0'}, 0, function(){
